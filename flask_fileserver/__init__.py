21fc27f5d30201eb4c51eeae09f2b1203e767fe1
rivergold <jinghe.rivergold@gmail.com>
2022-03-26 15:55:59 +0800

    Add upload feature

--- flask_fileserver/__init__.py
+++ flask_fileserver/__init__.py
@@ -1,8 +1,18 @@
+from crypt import methods
 import os.path
 from flask import Flask
 from flask_autoindex import AutoIndex
+from .route_func.upload import upload_file
 
-def create_app(browse_root_dir=None):
-     app = Flask(__name__)
-     AutoIndex(app, browse_root=browse_root_dir.as_posix())
-     return app
+
+def create_app(browse_root_dir=None, upload_dir=None):
+    app = Flask(__name__)
+    AutoIndex(app, browse_root=browse_root_dir.as_posix())
+    app.config['upload_dir'] = upload_dir
+    app.add_url_rule('/upload',
+                     endpoint='upload',
+                     view_func=upload_file,
+                     methods=['GET', 'POST'])
+    for rule in app.url_map.iter_rules():
+        print(rule)
+    return app
